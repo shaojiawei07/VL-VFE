6a9443d5d180de16470793c53f3b4ebfd60aeac3
shaojiawei07 <57165574+shaojiawei07@users.noreply.github.com>
2021-09-06 16:48:02 +0800

    Update VL_VFE_CIFAR.py

--- VL_VFE_CIFAR.py
+++ VL_VFE_CIFAR.py
@@ -1,4 +1,3 @@
-import os
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -6,55 +5,45 @@ import torchvision
 from torchvision import transforms
 from torchvision.utils import save_image
 from torch.optim.lr_scheduler import StepLR
-import numpy as np
 import argparse
-import random
 import copy
-#import resnet
-#import compression_resnet
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-parser = argparse.ArgumentParser()
-parser.add_argument('--intermediate_dim', type=int, default=64, help='dimension of encoded vector')
+parser = argparse.ArgumentParser(description='PyTorch CIFAR Example')
+parser.add_argument('--intermediate_dim', type=int, default=64)
 parser.add_argument('--epochs', type=int, default=320, help='epochs')
-parser.add_argument('--batch', type=int, default=128, help='batch size')
-parser.add_argument('--lr', type=float, default=1e-3, help='leaerning rate')
-parser.add_argument('--gamma', type=float, default=0.5, metavar='M',
-                    help='Learning rate step gamma (default: 0.7)')
-parser.add_argument('--penalty', type=float, default=1e-2)
-parser.add_argument('--thr_ratio', type=float, default=1e-2)
-parser.add_argument('--init_gamma', type=float, default=0)
+parser.add_argument('--batch_size', type=int, default=128, help='batch size')
+parser.add_argument('--lr', type=float, default=1e-3, help='learning rate')
+parser.add_argument('--gamma', type=float, default=0.5)
+parser.add_argument('--beta', type=float, default=1e-2)
+parser.add_argument('--threshold', type=float, default=1e-2)
 parser.add_argument('--decay_step', type=int, default=60)
 parser.add_argument('--test', type=int, default=0)
 parser.add_argument('--weights', type=str)
 parser.add_argument('--channel_noise', type=float, default = 0.1)
 args = parser.parse_args()
 
-# Device configuration†
+# Device Configuration
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+# Dataset
 transform_train = transforms.Compose([
     transforms.RandomCrop(32, padding=4),
     transforms.RandomHorizontalFlip(),  
     transforms.ToTensor(),
     transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
 ])
-
 transform_test = transforms.Compose([
     transforms.ToTensor(),
     transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
 ])
-
 trainset = torchvision.datasets.CIFAR10(root='./CIFAR10', train=True, download=True, transform=transform_train)
-
 testset = torchvision.datasets.CIFAR10(root='./CIFAR10', train=False, download=True, transform=transform_test)
 test_loader_this  = torch.utils.data.DataLoader(testset, batch_size=1000, shuffle=False, num_workers=2)
 
 
 def seed_torch(seed=0):
-    #random.seed(seed)
-    os.environ['PYTHONHASHSEED'] = str(seed)
-    np.random.seed(seed)
     torch.manual_seed(seed)
     torch.cuda.manual_seed(seed)
     torch.cuda.manual_seed_all(seed)
@@ -147,6 +136,7 @@ class Net(nn.Module):
                     nn.BatchNorm2d(512),
                     nn.ReLU()
                     )
+
         self.classifier1 = nn.Sequential(
                     nn.MaxPool2d(kernel_size = 4, stride = 4, padding = 0, dilation = 1, ceil_mode = False),
                     Flatten()
@@ -165,6 +155,7 @@ class Net(nn.Module):
                         nn.Linear(64,64),
                         nn.Sigmoid()
                         )
+
         self.encoder3_weight = nn.Parameter(torch.Tensor(self.hidden_channel, 64))
         self.encoder3_bias = nn.Parameter(torch.Tensor(self.hidden_channel))
         self.encoder3_weight.data.normal_(0, 0.5)
@@ -197,7 +188,6 @@ class Net(nn.Module):
         self.gamma_mu = gamma_function().to(device)
         self.upper_tri_matrix = torch.triu(torch.ones((args.intermediate_dim,args.intermediate_dim))).to(device)
 
-
     def forward(self, x, epoch, noise = 0.1):
 
         x = self.prep(x)
@@ -209,6 +199,7 @@ class Net(nn.Module):
         x = self.encoder1(x)
         x = torch.reshape(x,(x.size()[0],4*4*4))
 
+        # Dynamic Channel Conditions
         if self.training:
             channel_noise = torch.rand(1)*0.27 + 0.05
         else:
@@ -224,45 +215,27 @@ class Net(nn.Module):
         bias3 = torch.clamp(torch.abs(bias3),min = 1e-3) * torch.sign(bias3.detach())
         l2_norm_squared = torch.sum(weight3.pow(2),dim = 1) + bias3.pow(2)
         l2_norm = l2_norm_squared.pow(0.5)
-        #if torch.min(l2_norm) < 1e-6:
-        #    print(l2_norm)
         weight3 = (weight3.permute(1,0) / (l2_norm+1e-6)).permute(1,0)
         bias3 = bias3 / (l2_norm+1e-6)
         x = F.linear(x, weight3, bias3)
-        mu = self.gamma_mu(channel_noise)
-        '''
-        if self.training and torch.max(torch.isnan(mu)) == 1 and self.nan_flag ==0:
-            print(mu)
-            print('mu is nan')
-            self.nan_flag = 1
-            print(error)
-        '''
 
+        mu = self.gamma_mu(channel_noise)
         mu = F.linear(mu, self.upper_tri_matrix)
         mu = torch.clamp(mu,min = 1e-4)
         encoded_feature = torch.tanh(x * mu)
         encoded_feature = torch.clamp(torch.abs(encoded_feature),min = 1e-2) * torch.sign(encoded_feature.detach())
-        #raw_x_mask = self.get_mask(torch.abs(encoded_feature),threshold = 1e-2)
         
-
         # KL divergence
         KL = self.KL_log_uniform(channel_noise,torch.abs(encoded_feature))
-        '''
-        if self.training and torch.max(torch.isnan(KL)) == 1 and self.nan_flag == 0:
-            print(KL)
-            print('KL is nan')
-            self.nan_flag = 1
-            print(error)
-        '''
 
+        # Gaussian channel noise
         x = encoded_feature + torch.randn_like(encoded_feature) * channel_noise
 
         if self.training:
             if epoch > 60:
-                x = x * self.get_mask(mu,threshold = args.thr_ratio)
-            #pass
+                x = x * self.get_mask(mu,threshold = args.threshold)
         else:
-            x = x * self.get_mask(mu,threshold = args.thr_ratio)
+            x = x * self.get_mask(mu,threshold = args.threshold)
 
         x = F.relu(self.decoder1(x))
         x = self.decoder1_2(x)
@@ -280,9 +253,8 @@ class Net(nn.Module):
         return output, KL * 0.1 / channel_noise
 
     def KL_log_uniform(self,channel_noise,encoded_feature):
-        #alpha_squared = (sigma/mu)^(2)
+
         alpha = (channel_noise/encoded_feature)
-        #print(torch.min(alpha),torch.min(torch.log(alpha)))
         k1 = 0.63576
         k2 = 1.8732
         k3 = 1.48695
@@ -290,14 +262,12 @@ class Net(nn.Module):
         KL_term = k1 * F.sigmoid(k2 + k3 * 2 * torch.log(alpha)) - 0.5 * F.softplus(-2 * torch.log(alpha)) - k1
         return - torch.sum(KL_term) / batch_size
 
-    def get_mask(self, mu, threshold=args.thr_ratio):
-
-        #alpha = F.linear(torch.abs(mu), self.upper_tri_matrix)
+    def get_mask(self, mu, threshold=args.threshold):
         alpha = mu.detach()
         hard_mask = (alpha > threshold).float()
         return hard_mask
 
-    def get_mask_inference(self, channel_noise, threshold = args.thr_ratio):
+    def get_mask_inference(self, channel_noise, threshold = args.threshold):
         mu = self.gamma_mu(channel_noise)
         alpha = F.linear(mu, self.upper_tri_matrix)
         hard_mask = (alpha > threshold).float()
@@ -307,34 +277,17 @@ model = Net(args).to(device)
 optimizer = torch.optim.Adam(model.parameters(), lr=args.lr,  weight_decay=1e-4)
 scheduler = StepLR(optimizer, step_size=args.decay_step, gamma=args.gamma)
 
-'''
-
-def inference_accuracy(output, labels):
-    preds = output.max(1)[1].type_as(labels)  
-    correct = preds.eq(labels).double()
-    correct = correct.sum()
-    return correct / len(labels)
-'''
-
-# Start training
 def train(model=model):
     
-    #flag = 0
     test_acc = 0
-    #save_mask_dim = 0
     pruned_dim = 0
-    test_acc2 = 0
-    pruned_number2 = 0
-    saved_model1 = {}
-    saved_model2 = {}
-    last_param = []
-    last_param_grad = []
+    saved_model = {}
 
     for epoch in range(args.epochs):
 
-        print('----epoch:{}----'.format(epoch))
+        print('\nepoch:{}'.format(epoch))
         if (epoch)%10 == 0:
-            data_loader = torch.utils.data.DataLoader(trainset,batch_size=args.batch, shuffle=True,
+            data_loader = torch.utils.data.DataLoader(trainset,batch_size=args.batch_size, shuffle=True,
         num_workers=4, pin_memory=True)
 
         for i, (x, y) in enumerate(data_loader):
@@ -349,119 +302,59 @@ def train(model=model):
                 loss = loss1
             else:
                 anneal_ratio = min(1,(epoch - 20)/20)
-                loss = loss1 + args.penalty * KL * anneal_ratio
-
+                loss = loss1 + args.beta * KL * anneal_ratio
 
             if torch.isnan(loss):
-                #print('loss')
                 raise Exception("NaN value")
 
-
             optimizer.zero_grad()
-
             loss.backward()
-
-            #if KL > 300:
-                #print(KL)
-
-            '''
-
-            if i == 0:
-                parameters = model.parameters()
-                if isinstance(parameters, torch.Tensor):
-                    print('true')
-                    parameters = [parameters]
-                parameters = [p for p in parameters if p.grad is not None]
-                max_mag = max(p.grad.detach().abs().max().to(device) for p in parameters)
-                total_norm = torch.norm(torch.stack([torch.norm(p.grad.detach(), 2).to(device) for p in parameters]), 2)
-                nan_value = torch.max(torch.stack([torch.any(torch.isnan(p.grad.detach())).to(device) for p in parameters]))
-                nan_weight_value = torch.max(torch.stack([torch.max(torch.isnan(p.detach())).to(device) for p in parameters]))
-                max_weight = max(p.detach().abs().max().to(device) for p in parameters)
-                min_weight = min(p.detach().abs().max().to(device) for p in parameters)
-                #print('max magnitude:{:.4f}'.format(max_mag.item()),'l2 norm:{:.4f}'.format(total_norm.item()),'grad_nan',nan_value.item(),'weight_nan',nan_weight_value.item(),'weight_max:{:.4f}_min:{:.10f}'.format(max_weight,min_weight))
-                #print('KL:{:.4f}'.format(KL.item()),'max magnitude:{:.4f}'.format(max_mag.item()),'l2 norm:{:.4f}'.format(total_norm.item()),'grad_nan',nan_value.item(),'weight_nan',nan_weight_value.item(),'weight_max:{:.4f}_min:{:.10f}'.format(max_weight,min_weight))
-                print('KL:{:.4f}'.format(KL.item()),'max magnitude:{:.4f}'.format(max_mag.item()),'l2 norm:{:.4f}'.format(total_norm.item()))
-            '''
-
             optimizer.step()
-            
-            '''
-            parameters = model.parameters()
-            parameters = [p for p in parameters if p.grad is not None]
-            nan_weight_value = torch.max(torch.stack([torch.max(torch.isnan(p.detach())).to(device) for p in parameters]))
-
-            if nan_weight_value == True:
-                for i, p in enumerate(parameters):
-                    print(i,p.size(),torch.max(torch.isnan(p)).item(),torch.max(torch.abs(p)).item(),torch.min(torch.abs(p)).item())
-                    print(i,torch.max(torch.isnan(p.grad)),torch.max(torch.abs(p.grad)))
-                print('------last batch--------')
-                for i, p in enumerate(last_param):
-                    print(i,p.size(),torch.max(torch.isnan(p)).item(),torch.max(torch.abs(p)).item(),torch.min(torch.abs(p)).item())
-            '''
-
-            #last_param = copy.deepcopy(parameters)
         scheduler.step()
-        #accuracy_result = inference_accuracy(output,y)
 
         acc, pruned_number = test(epoch,noise = 0.1)
+
+        print('Test Accuracy:',acc,'Pruned dim:',pruned_number,'Activated dim:', args.intermediate_dim - pruned_number)
+
         if epoch > 180:
             if (acc > test_acc and pruned_number == pruned_dim) or pruned_number > pruned_dim:
                 test_acc = acc
                 pruned_dim = pruned_number
-                saved_model1 = copy.deepcopy(model.state_dict())
-                print('best ckpt:',test_acc,'number:',args.intermediate_dim,'pruned_number',pruned_dim,'beta:',args.penalty,'threshold:',args.thr_ratio)
-                torch.save({'model': saved_model1}, './model_cifar/VL_VFE_dynamic_best_ckpt_hid:{}_penalty:{}_model1.pth'.format(args.intermediate_dim,args.penalty))
-    print()
-    print('best acc:',test_acc,'number:',args.intermediate_dim,'pruned_number',pruned_dim,'beta:',args.penalty,'threshold:',args.thr_ratio)
-    torch.save({'model': saved_model1}, './model_cifar/VL_VFE_dynamic_hid:{}_remain:{}_penalty:{}_acc:{}_epoch:{}_model1.pth'.format(args.intermediate_dim,args.intermediate_dim - pruned_dim,args.penalty,test_acc,epoch))
+                saved_model = copy.deepcopy(model.state_dict())
+                print('Best ckpt:',test_acc,'pruned_number:',pruned_dim,'beta:',args.beta)
+                torch.save({'model': saved_model}, './CIFAR_best_ckpt_dim:{}_beta:{}_model.pth'.format(args.intermediate_dim,args.beta))
+    print('Best Accuracy:',test_acc,'Intermediate Dim:',args.intermediate_dim,'Beta:',args.beta)
+    torch.save({'model': saved_model}, './CIFAR_model_dim:{}_beta:{}_accuracy:{}_model.pth'.format(args.intermediate_dim - pruned_dim,args.beta,test_acc))
 
 def test(epoch,noise=0.1):
     with torch.no_grad():
         model.eval()
-
         correct = 0
-        #correct_top5 = 0
         total = 0
-
         for i, (images, labels) in enumerate(test_loader_this): 
             images = images.to(device)
             labels = labels.to(device)
             outputs,_= model(images,epoch,noise)
-            maxk = max((1,5))
             labels_relize = labels.view(-1,1)
             _, predicted = torch.max(outputs.data, 1)
             total += labels.size(0)
             correct += (predicted == labels).sum().item()
-
         hard_mask, mu = model.get_mask_inference(torch.FloatTensor([noise]).to(device))
-
         index = torch.nonzero(torch.lt(hard_mask,0.5)).squeeze(1)
-
         pruned_number = index.size()[0]
-
-        print(mu)
-
-        print('pruned alpha number:',pruned_number)
-
-        print('Accuracy of the network on the 10000 test images: {} %'.format(100 * correct / total))
-
         return 100 * correct / total, pruned_number
-
-
-
-
       
 if __name__=='__main__':
     seed_torch(0)
     if args.test == 0:
-    	train(model)
+        train(model)
     else:
         model.load_state_dict(torch.load(args.weights)['model'])
-        avg = 0
+        accuracy = 0
         t = 20
         for i in range (t):
-            acc, _ = test(0,args.channel_noise)
-            avg += acc
-        print('Noise:',args.channel_noise,'Accuracy:',avg/t)
-
+            acc, pruned_number = test(0,args.channel_noise)
+            accuracy += acc
+        #print('Noise level:',args.channel_noise,'Test Accuracy:',accuracy/t,'Activated dim:', args.intermediate_dim - pruned_number)
+        print('Noise level:',args.channel_noise, 'Test Accuracy:', accuracy/t, 'Pruned dim:', pruned_number, 'Activated dim:', args.intermediate_dim - pruned_number)
 
