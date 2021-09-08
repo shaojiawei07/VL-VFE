a85ed2f6e146eb41eb53db0bd6b23d5e7aeafd99
shaojiawei07 <57165574+shaojiawei07@users.noreply.github.com>
2021-09-08 13:10:26 +0800

    Update VL_VFE_MNIST.py

--- VL_VFE_MNIST.py
+++ VL_VFE_MNIST.py
@@ -222,7 +222,7 @@ def main_train():
                 pruned_dim = pruned_number
                 saved_model = copy.deepcopy(model.state_dict())
     print('Best Accuray:',test_acc,'pruned_number:',pruned_dim,'activated_dim:',args.intermediate_dim - pruned_dim)
-    torch.save({'model': saved_model}, './MNIST_model_dim:{}_beta:{}_accuracy:{:.4f}_model1.pth'.format(args.intermediate_dim - pruned_dim,args.beta, test_acc))
+    torch.save({'model': saved_model}, './MNIST_model_dim:{}_beta:{}_accuracy:{:.4f}_model.pth'.format(args.intermediate_dim - pruned_dim,args.beta, test_acc))
 
 def main_test():
     kwargs = {'num_workers': 1, 'pin_memory': True}
